fe86d4cc7e2a67aecfcccaaac079c63adf991346
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2021-07-22 15:31:23 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function BookList() {
+  return (
+    <section>
+      <Book />      
+    </section>
+  )
+}
+
+const Book = () => {
+   return (
+     <article>
+       <Image />
+       <Image />
+       <Image />
+     </article>
+   )
+}
+
+const Image = () => <img src = "https://m.media-amazon.com/images/I/714PYe6ugmL._AC_UY218_.jpg" alt= '' />
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
