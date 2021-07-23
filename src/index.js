6dd58f4c62deb6dcb7ab2928a6f63e7663ded791
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2021-07-23 15:23:39 +0000

    Implemented props

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import './index.css'
+
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/714PYe6ugmL._AC_UY218_.jpg',
+  title: 'High Performance Habits',
+  author: 'Brendon Burchard'
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/81SrwYY-6-L._AC_UY218_.jpg',
+  title: 'The Lean Startup',
+  author: 'Eric Ries'
+}
+
 function BookList() {
   return (
-    <section>
-      <Book />      
+    <section className='booklist'>
+      <Book img = {firstBook.img} title = {firstBook.title} author = {firstBook.author} />
+      <Book img = {secondBook.img} title = {secondBook.title} author = {secondBook.author} />
     </section>
   )
 }
 
-const Book = () => {
-   return (
-     <article>
-       <Image />
-       <Image />
-       <Image />
-     </article>
-   )
-}
 
-const Image = () => <img src = "https://m.media-amazon.com/images/I/714PYe6ugmL._AC_UY218_.jpg" alt= '' />
+const Book = ({img, title, author}) => {
+  // console.log(props)
+  return (
+    <article className='book'>
+    <img src = {img} alt = '' />
+    <h1> {title} </h1>
+    <h4> {author} </h4>
+    </article>
+  )
+}
 
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
